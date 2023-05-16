98343b3286337f18c50b5cacd7c59facaca70415
Ryan Eastman <rking1323@gmail.com>
2023-05-15 18:05:49 -0700

    added config files from class repo

---
+++ index.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+// Create Express Singleton - a single instance by design
+const app = express();
+
+
+
+
