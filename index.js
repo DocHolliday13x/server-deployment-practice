8ee1ba536c35e00f05d2b1ed4bbd80fc6bd81f46
Ryan Eastman <rking1323@gmail.com>
2023-05-15 18:24:42 -0700

    testing proof of life on local host 3001

--- index.js
+++ index.js
@@ -3,9 +3,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 // Create Express Singleton - a single instance by design
 const app = express();
+const PORT = process.env.PORT;
 
+// Middleware - a function that runs before the route handler
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Proof of Life');
+});
+
+app.listen(PORT, () => console.log('Server up on', PORT));
 
 
 
